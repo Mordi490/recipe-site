beaf30e4f9896bb607318341105f6c2005c5f3fc
Mordi490 <jananton007@hotmail.com>
2023-08-24 13:11:42 +0200

    added prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // consider having a dark mode and a amoled mode
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
