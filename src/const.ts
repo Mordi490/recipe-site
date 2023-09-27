08ebaf2e48ac9c111f5be0d18b5b80f782179d58
Mordi490 <jananton007@hotmail.com>
2023-09-27 21:16:45 +0200

    This git hisotry is gonna be great

--- src/const.ts
+++ src/const.ts
@@ -2,3 +2,6 @@
 export const title = "Bentes oppskrifter";
 export const desc = "Arkiv av oppskrifter";
 export const PER_PAGE = 70;
+export const BASE_RECIPE_URL = import.meta.env.PROD
+  ? "bentesoppskrifter.no"
+  : "localhost:4321";
