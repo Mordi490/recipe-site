dcf53f11ec00519d0bd3271f84ffd9433ef99baa
Mordi490 <jananton007@hotmail.com>
2023-09-28 23:49:57 +0200

    json endpoint for recipes

---
+++ src/pages/search.json.js
@@ -0,0 +1,20 @@
+import { getCollection } from "astro:content";
+
+async function getOppskrifter() {
+  const oppskrifter = await getCollection("oppskrifter");
+  return oppskrifter.map((o) => ({
+    slug: o.slug,
+    title: o.data.title,
+    ingredienser: o.data.ingredienser,
+    tattFra: o.data.tattFra,
+  }));
+}
+
+export async function get({}) {
+  return new Response(JSON.stringify(await getOppskrifter()), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
