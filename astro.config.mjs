4ff994878067d1a7657d27f21cdd2e81f03d25b9
Mordi490 <jananton007@hotmail.com>
2023-10-31 18:45:05 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bentesoppskrifter.no",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "hybrid",
   adapter: vercel(),
-  imageService: true,
+  imageService: true
 });
