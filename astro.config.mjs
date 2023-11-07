770b3d372082d5212ce11626101728aff0a215ea
Mordi490 <jananton007@hotmail.com>
2023-11-08 00:49:50 +0100

    dumb og:image approach

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,33 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [rawFonts([`.ttf`])],
+    optimizeDeps: { exclude: [`@resvg/resvg-js`] },
+  },
   site: "https://www.bentesoppskrifter.no",
   integrations: [tailwind(), sitemap()],
   output: "hybrid",
   adapter: vercel(),
-  imageService: true
+  imageService: true,
 });
+
+// Vite will try to optimize fonts, and it doesn't have to
+function rawFonts(ext) {
+  return {
+    name: "vite-plugin-raw-fonts",
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
+}
