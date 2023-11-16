514fe54c514254845307ea074eed61ef8b2476d8
Mordi490 <jananton007@hotmail.com>
2023-11-16 22:48:48 +0100

    Remove silly const

--- src/const.ts
+++ src/const.ts
@@ -2,6 +2,3 @@
 export const title = "Bentes oppskrifter";
 export const desc = "Arkiv av oppskrifter";
 export const PER_PAGE = 70;
-export const BASE_RECIPE_URL = import.meta.env.PROD
-  ? "https://www.bentesoppskrifter.no"
-  : "http://localhost:4321";
