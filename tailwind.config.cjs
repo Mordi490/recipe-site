c1a79df70cbe84553e537c5302cfbf41e21ddc0f
Mordi490 <jananton007@hotmail.com>
2024-09-30 16:13:57 +0200

    font chnage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        atkinson: ["Atkinson-Hyperlegible", "system-ui", "sans-serif"],
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
