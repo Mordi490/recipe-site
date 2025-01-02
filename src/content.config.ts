7d615a53773ad16a1280c7e8741fa6f20829200f
Mordi490 <jananton007@hotmail.com>
2025-01-02 02:26:13 +0100

    followed migration guide, id is the new slug

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,11 @@
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const oppskrifter = defineCollection({
-  type: "content",
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./src/content/oppskrifter",
+  }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
--- src/pages/search.json.js
+++ src/pages/search.json.js
@@ -3,7 +3,7 @@ import { getCollection } from "astro:content";
 async function getOppskrifter() {
   const oppskrifter = await getCollection("oppskrifter");
   return oppskrifter.map((o) => ({
-    slug: o.slug,
+    id: o.id,
     title: o.data.title,
     ingredienser: o.data.ingredienser,
     tattFra: o.data.tattFra,
