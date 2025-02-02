15034b43890056367816d1f1eff3e146cbb4a3e0
Mordi490 <jananton007@hotmail.com>
2025-02-02 23:44:47 +0100

    actual use of "markdownit"

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,9 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import sanitizeHtml from "sanitize-html";
+import markdownit from "markdown-it";
 
-const parser = new MarkdownIt();
+const parser = markdownit();
 
 export async function GET(context) {
   const oppskrifter = await getCollection("oppskrifter");
@@ -14,6 +15,7 @@ export async function GET(context) {
     items: oppskrifter.map((o) => ({
       title: o.data.title,
       link: `/oppskrifter/${o.id}`,
+      description: `Oppskrift for ${o.data.title}`,
       content: sanitizeHtml(parser.render(o.body), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
       }),
