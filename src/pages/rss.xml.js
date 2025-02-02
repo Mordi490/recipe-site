a7674f0ed161ae3414370dcb6f7be572179104c9
Mordi490 <jananton007@hotmail.com>
2025-02-02 23:33:11 +0100

    render and sanitize mardown for rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const oppskrifter = await getCollection("oppskrifter");
@@ -11,7 +14,9 @@ export async function GET(context) {
     items: oppskrifter.map((o) => ({
       title: o.data.title,
       link: `/oppskrifter/${o.id}`,
-      //description: o.body,
+      content: sanitizeHtml(parser.render(o.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
     })),
     customData: `<language>no</language>
       <image>
